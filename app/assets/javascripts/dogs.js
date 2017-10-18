c992360d23f04cbd39086a9b1f5da773dca94031
KA Green <greenkg@gmail.com>
2017-10-18 17:04:50 +0200

    Have jquery start on turbolinks load

--- app/assets/javascripts/dogs.js
+++ app/assets/javascripts/dogs.js
@@ -1,7 +1,7 @@
 //= require_self
 //= require_tree .
 
-$(document).ready(function(){
+document.addEventListener("turbolinks:load", function(){
 
   $("#selector-button").click(function(){
     $("#breed-selector").slideDown();
